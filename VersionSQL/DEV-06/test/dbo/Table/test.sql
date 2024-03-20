742fcbf0694b5d5695e1422e2bd48fdc40a7385e
unknown <unknown>
2024-03-20 16:56:38 -0400

    tes 1

---
+++ VersionSQL/DEV-06/test/dbo/Table/test.sql
@@ -0,0 +1,10 @@
+﻿/****** Object:  Table [dbo].[test]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE TABLE [dbo].[test](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](40) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
